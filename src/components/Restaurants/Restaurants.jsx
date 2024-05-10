8c669e50b27174ed3731f47d9984fc24605230a1
Rivansh <rivansh63@gmail.com>
2024-05-10 14:52:21 +0530

    Merging in one

---
+++ src/components/Restaurants/Restaurants.jsx
@@ -0,0 +1,10 @@
+const Restaurants = () => {
+    return (
+        <>
+            <div className="flex p-4 mt-[6%]">
+                
+            </div>
+        </>
+    )
+}
+export default Restaurants;
